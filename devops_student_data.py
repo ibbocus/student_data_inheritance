7ce3d6afbdf969030e9225d887f061e66bdf4e5a
ibbocus <ib_bocus@hotmail.com>
2020-07-06 09:11:10 +0100

    update

---
+++ devops_student_data.py
@@ -0,0 +1,12 @@
+from student_data import Student
+
+class Devops(Student):
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def full_name(self):
+        print(self.first_name, " ", self.last_name)
+
+    def roll_call(self):
+        print("I am a Devops Student")
